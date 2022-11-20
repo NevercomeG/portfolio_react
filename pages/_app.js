ef651e9a2db083d6f595045cf220b6906d74874c
NevercomeG <luisjavier2012@gmail.com>
2022-11-20 16:39:28 -0400

    2.0.3 Moved to nextjs

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
 import React from "react";
 
-import Navbar from "../components/bcomponents/navbar.jsx";
-import Footer from "../components/bcomponents/footer.jsx";
+import Navbar from "./components/bcomponents/navbar.jsx";
+import Footer from "./components/bcomponents/footer.jsx";
+
 
 export default function Landing() {
   return (
