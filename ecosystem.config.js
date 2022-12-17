449a8a9530d6a558c1ae84ddd74d2729bc54e918
NevercomeG <luisjavier2012@gmail.com>
2022-12-17 18:48:05 -0400

    2.2.5

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: "nextjs-app",
+      name: "app1",
       script: "yarn",
+      args: "start",
       watch: true,
       env: {
         NODE_ENV: "development",
@@ -11,6 +12,20 @@ module.exports = {
       env_production: {
         NODE_ENV: "production",
         PORT: 3000,
+      },
+    },
+    {
+      name: "app2",
+      script: "yarn",
+      args: "start",
+      watch: true,
+      env: {
+        NODE_ENV: "development",
+        PORT: 3001,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3001,
       },
     },
   ],
