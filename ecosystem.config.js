57553fb3142b6be5cf19fe3b66afdef79035e426
NevercomeG <luisjavier2012@gmail.com>
2022-12-17 18:00:04 -0400

    2.2.4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "nextjs-app",
-      script: "./server.js",
+      script: "yarn",
       watch: true,
       env: {
         NODE_ENV: "development",
