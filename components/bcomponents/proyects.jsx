8482c8a52f3ded9ccd5969309fc8b6d20507e7fd
NevercomeG <luisjavier2012@gmail.com>
2022-12-19 18:45:10 -0400

    2.3.4

--- components/bcomponents/proyects.jsx
+++ components/bcomponents/proyects.jsx
@@ -7,6 +7,9 @@ export default function formation() {
         <h1 className="text-white font-bold text-5xl shadow-lg">
           Github Projects
         </h1>
+        <h4 className="text-white font-thin text-2xl shadow-lg">
+          working in progress
+        </h4>
         <div className="formation__wrapper">
           <div className="formation__card"></div>
           <div className="formation__card"></div>
