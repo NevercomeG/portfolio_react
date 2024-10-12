329987aa4ca4d80d281fc3745b07b2c7ee832437
nevercomex <luisjavier2012@gmail.com>
2024-10-12 14:01:00 -0400

    fix

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -1,8 +1,12 @@
+'use client';
+
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { MDXRemote } from 'next-mdx-remote';
 import { serialize } from 'next-mdx-remote/serialize';
 import MDXComponents from '@/components/MDXComponents';
+import NavBarSection from '@/components/Navs';
+import FooterSection from '@/components/Footer';
 
 export default function PostPage() {
   const router = useRouter();
@@ -19,7 +23,7 @@ export default function PostPage() {
       try {
         // Cambia esta URL para apuntar a tu repositorio de GitHub
 
-        const GITHUB_RAW_URL = `https://raw.githubusercontent.com/NevercomeX/projectsmdx/refs/heads/main/escuela-red-pill.mdx`;
+        const GITHUB_RAW_URL = `https://raw.githubusercontent.com/NevercomeX/projectsmdx/refs/heads/main/${slug}.mdx`;
 
         const response = await fetch(GITHUB_RAW_URL);
         if (!response.ok) throw new Error('Post not found');
@@ -45,9 +49,12 @@ export default function PostPage() {
   }
 
   return (
-    <div>
-      {/* Renderiza el contenido serializado del MDX */}
-      <MDXRemote {...source} components={MDXComponents} />
-    </div>
+    <>
+      <NavBarSection />
+      <div className='px-8 sm:px-24 md:px-52 lg:px-1/8 flex flex-col justify-center'>
+        <MDXRemote {...source} components={MDXComponents} />
+      </div>
+      <FooterSection />
+    </>
   );
 }
