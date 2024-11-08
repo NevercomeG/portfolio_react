90d29556a23be4db578fd774d95a1ab73ec84b2c
nevercomex <luisjavier2012@gmail.com>
2024-11-08 17:12:38 -0400

    feat: go up bottom

---
+++ src/components/Navs/sectionNavigation.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { FaArrowUp } from 'react-icons/fa';
+
+export default function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <>
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          className='fixed bottom-8 right-8 z-50 p-3 bg-green-500 text-white rounded-full shadow-lg hover:bg-green-700 transition duration-300'
+          aria-label='Go to top'
+        >
+          <FaArrowUp className='h-6 w-6' />
+        </button>
+      )}
+    </>
+  );
+}
